12515b9edadb05f791383516635946cd8a6499b9
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-12-14 20:11:36 +0600

    Dependencies problem fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 
     // Coroutine Lifecycle Scopes
@@ -88,8 +88,12 @@ dependencies {
     kapt("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    
+
     //Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.39")
+    implementation("com.google.dagger:hilt-android:2.48")
 
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
